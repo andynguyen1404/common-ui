ae53598ac8441d9f50fc400d27fa6ae6f4db0a9c
andn <an.d.nguyen.411@gmail.com>
2024-03-31 01:06:53 +0700

    Update package json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.jsx',
     output: {
-        filename: 'main.js',
+        filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
